7370b7af1b47c27c7eb550fcadd138ecfb232027
Noel Wilson <jwnwilson@hotmail.co.uk>
2016-12-10 15:49:51 +0000

    Basic chat room working

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
-var io = require('socket.io');
+var socket = require('socket.io');
 
 var app = express();
 var port = 3000;
@@ -13,12 +13,19 @@ app.engine('.hbs', exphbs({
 }));
 app.set('view engine', '.hbs');
 //app.enable('view cache');
-app.use(express.static(__dirname + '/static'));
+app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', function(req, res){
     res.render('home');
 });
 
 //app.listen(port);
-io.listen(app.listen(port));
+var io = socket.listen(app.listen(port));
 console.log('Listening on port ' + port);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
