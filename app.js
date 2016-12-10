3ef6ad4678b3f5fc4239b134b3668ae03a7f823c
Noel Wilson <jwnwilson@hotmail.co.uk>
2016-12-10 14:40:38 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var io = require('socket.io');
+
+var app = express();
+var port = 3000;
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir='views/layouts/',
+  partialsDir='views/partials/'
+}));
+app.set('view engine', '.hbs');
+//app.enable('view cache');
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+//app.listen(port);
+io.listen(app.listen(port));
+console.log('Listening on port ' + port);
